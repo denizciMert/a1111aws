73fb5bdd9783e7ac3ea411b1b59b4e8c832dbfeb
denizciMert <153491385+denizciMert@users.noreply.github.com>
2024-01-04 02:20:38 +0300

    Update a1111aws-fs.sh

--- a1111aws-fs.sh
+++ a1111aws-fs.sh
@@ -3,7 +3,7 @@
 echo "################################################################"
 echo "Kurulum başliyor!"
 echo "################################################################"
-
+echo ""
 # Sistem güncellemeleri
 sudo yum update -y
 
@@ -11,9 +11,11 @@ sudo yum update -y
 sudo yum install -y git wget
 
 # Gerekli kütüphanelerin kurulumu
+echo ""
 echo "################################################################"
 echo "Gereksinimler kuruluyor."
 echo "################################################################"
+echo ""
 
 sudo yum install wget git python3 gperftools-libs libglvnd-glx
 
@@ -22,37 +24,90 @@ mkdir -p ~/A1111
 cd ~/A1111
 
 # Automatic1111 reposunu klonlama
+echo ""
 echo "################################################################"
 echo "WebUI indiriliyor."
 echo "################################################################"
+echo ""
 
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 
 # Modellerin ve eklentilerin indirilmesi
+echo ""
 echo "################################################################"
 echo "Modeller indiriliyor."
 echo "################################################################"
+echo ""
 
 cd stable-diffusion-webui/models/Stable-diffusion
+
+echo ""
+echo "################################################################"
+echo "Model - Realistic Vision V6.0 B1"
+echo "################################################################"
+echo ""
 wget https://civitai.com/api/download/models/245598 --content-disposition
+
+echo ""
+echo "################################################################"
+echo "Model - epiCRealism RC1"
+echo "################################################################"
+echo ""
 wget https://civitai.com/api/download/models/143906 --content-disposition
 
 cd ..
 
+echo ""
 echo "################################################################"
 echo "Upscaler indiriliyor."
 echo "################################################################"
+echo ""
 
-mkdir ESGRAN
-cd ESGRAN
+mkdir ESRGAN
+cd ESRGAN
+echo ""
+echo "################################################################"
+echo "Upscaler - 4x-UltraSharp"
+echo "################################################################"
+echo ""
 wget https://huggingface.co/lokCX/4x-Ultrasharp/resolve/main/4x-UltraSharp.pth
 
 cd ../..
 
+echo ""
+echo "################################################################"
+echo "Eklentiler indiriliyor."
+echo "################################################################"
+echo ""
+
+cd extensions
+echo ""
+echo "################################################################"
+echo "Extension - Reactor"
+echo "################################################################"
+echo ""
+git clone https://github.com/Gourieff/sd-webui-reactor
+echo ""
+echo "################################################################"
+echo "Extension - Controlnet"
+echo "################################################################"
+echo ""
+git clone https://github.com/Mikubill/sd-webui-controlnet
+echo ""
+echo "################################################################"
+echo "Extension - Roop"
+echo "################################################################"
+echo ""
+git clone https://github.com/s0md3v/sd-webui-roop
+
+cd ..
+
 # WebUI kullanıcı yapılandırması
+echo ""
 echo "################################################################"
 echo "WebUI User Yapılandırması"
 echo "################################################################"
+echo ""
 
 read -p "Kullanici adini giriniz: " username
 read -sp "Sifreyi giriniz: " password
@@ -60,18 +115,23 @@ read -sp "Sifreyi giriniz: " password
 sed -i "s/^#export COMMANDLINE_ARGS=.*/export COMMANDLINE_ARGS=\"--share --gradio-auth $username:$password\"/" webui-user.sh
 
 # Çalıştırma izinlerinin yapılandırması
+echo ""
 echo "################################################################"
 echo "Calıstırma Izinleri Yapilandiriliyor."
 echo "################################################################"
+echo ""
 
 sudo chmod +x webui.sh
-
+echo ""
 echo "################################################################"
 echo "Kurulum tamamlandi!"
 echo "################################################################"
+echo ""
+echo ""
 echo "################################################################"
 echo "Ilk baslatma zaman alabilir!"
 echo "################################################################"
+echo ""
 
 # WebUI betiğini çalıştırma
 ./webui.sh -f
